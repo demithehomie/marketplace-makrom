f164df0ea781f002a32a91d0c3fe03dc1a26f275
Allaf <allaflima@hotmail.com>
2023-04-22 19:46:44 -0300

    a

--- Backend/src/common/auth/password-reset.controller.ts
+++ Backend/src/common/auth/password-reset.controller.ts
@@ -1,46 +1,84 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { EmailService } from './email.service';
-import { PrismaService } from '../prisma/prisma.service';
-import { compareSync } from 'bcrypt';
+import { Body, Controller, Post } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service';
+import { EmailService } from './email.service';
+import { SmsService } from './sms.service';
 
 @Controller()
 export class PasswordResetController {
+  private pendingCodes = {};
+
   constructor(
     private readonly emailService: EmailService,
     private readonly prismaService: PrismaService,
-  ) {}y
+    private readonly smsService: SmsService,
+  ) {}
 
-  @Post('auth/password-reset/request')
+  @Post('auth/password-reset/email-request')
   async requestPasswordReset(@Body() { email }) {
-    const user = await this.prismaService.getClient().client.findUnique({ where: { email } });
+    const user = await this.prismaService
+      .getClient()
+      .client.findUnique({ where: { email } });
     if (!user) {
       throw new Error('Usuário não encontrado');
     }
     const speakeasy = require('speakeasy');
     const token = speakeasy.totp({
       secret: speakeasy.generateSecret({ length: 20 }).base32,
-      digits: 6
+      digits: 6,
     });
     await this.prismaService.getClient().client.update({
       where: { email: user.email },
-      data: { resetPasswordToken: token, resetPasswordTokenExpiresAt: new Date(Date.now() + 3600000) }, // expires in 1 hour
+      data: {
+        resetPasswordToken: token,
+        resetPasswordTokenExpiresAt: new Date(Date.now() + 3600000),
+      }, // expires in 1 hour
     });
     await this.emailService.sendResetPasswordToken(user.email, token);
     return { message: 'Email enviado com instruções para redefinir a senha' };
   }
 
-  @Post('auth/password-reset/confirm')
+  @Post('auth/password-reset/email-confirm')
   async confirmPasswordReset(@Body() { token, password }) {
-    const user = await this.prismaService.getClient().client.findUnique({ where: { resetPasswordToken: token } });
+    const user = await this.prismaService
+      .getClient()
+      .client.findUnique({ where: { resetPasswordToken: token } });
     if (!user || user.resetPasswordTokenExpiresAt < new Date()) {
       throw new Error('Token inválido ou expirado');
     }
     const passwordHash = await bcrypt.hashSync(user.senha, 8);
     await this.prismaService.getClient().client.update({
-      where: { email: user.email,  },
-      data: { senha: passwordHash, resetPasswordToken: "0", resetPasswordTokenExpiresAt: new Date(Date.now()) },
+      where: { email: user.email },
+      data: {
+        senha: passwordHash,
+        resetPasswordToken: '0',
+        resetPasswordTokenExpiresAt: new Date(Date.now()),
+      },
     });
     return { message: 'Senha redefinida com sucesso' };
+  }
+
+  @Post('auth/password-reset/sms-request')
+  async sendVerificationCode(@Body() data: Sms): Promise<string> {
+    const phoneNumber = data.numero[0];
+    const verificationCode = await this.smsService.sendSms(data);
+    this.pendingCodes[phoneNumber] = verificationCode;
+    return 'Código de verificação enviado com sucesso.';
+  }
+
+  @Post('auth/password-reset/sms-confirm')
+  async validateVerificationCode(
+    @Body() payload: { phoneNumber: string; verificationCode: string },
+  ): Promise<{ valid: boolean }> {
+    const { phoneNumber, verificationCode } = payload;
+    const pendingCode = this.pendingCodes[phoneNumber];
+    if (pendingCode === verificationCode) {
+      // Código de verificação válido, limpar pendingCodes e retornar verdadeiro
+      delete this.pendingCodes[phoneNumber];
+      return { valid: true };
+    } else {
+      // Código de verificação inválido, retornar falso
+      return { valid: false };
+    }
   }
 }
---
+++ Backend/src/common/auth/sms.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+require('dotenv').config();
+const HASH_SMS_MOBILE = process.env.HASH_SMS_MOBILE;
+
+@Injectable()
+export class SmsService {
+  private pendingCodes: { [phoneNumber: string]: string } = {};
+
+  private generateVerificationCode(): string {
+    const randomNumber = Math.floor(Math.random() * 100000);
+    return randomNumber.toString().padStart(5, '0');
+  }
+
+  async sendSms(data: Sms): Promise<string> {
+    const verificationCode = this.generateVerificationCode();
+    const mensagem = `Seu código de verificação é ${verificationCode}. Não compartilhe com ninguém.`;
+    const response = await axios.post(
+      'https://app.websms.com.br/sms/shortcode/routes/sms.php',
+      {
+        mensagem,
+        acao: data.acao, //'enviar',
+        numero: data.numero, //'22992248416',
+        hash: HASH_SMS_MOBILE,
+      },
+    );
+    if (response.data === '{ status: "Enviado" }') {
+      return verificationCode;
+    } else {
+      throw new Error(`Failed to send SMS: ${response}`);
+    }
+  }
+
+  async validateVerificationCode(
+    phoneNumber: string,
+    verificationCode: string,
+  ): Promise<boolean> {
+    const pendingCode = this.pendingCodes[phoneNumber];
+    if (pendingCode === verificationCode) {
+      // Código de verificação válido, limpar pendingCodes e retornar verdadeiro
+      delete this.pendingCodes[phoneNumber];
+      return true;
+    } else {
+      // Código de verificação inválido, retornar falso
+      return false;
+    }
+  }
+}
